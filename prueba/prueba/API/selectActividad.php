0908c4df562bf35be3dbe99fedf8acb0ff2de0aa
plantajrach2 <50379624+plantajrach2@users.noreply.github.com>
2026-03-06 15:21:42 -0600

    Ajustes de estilo
    Se ajustaron estilos para que no se vieran desalineados incluyendo los CND's de Bootstrap.

---
+++ prueba/prueba/API/selectActividad.php
@@ -0,0 +1,5 @@
+<?php
+include '../class/classActividad.php';
+
+$option = new Actividad();
+$option->selectActividad();
---
+++ prueba/prueba/API/setActividad.php
@@ -0,0 +1,10 @@
+<?php
+include '../class/classActividad.php';
+
+if (isset($_POST)) {
+    $actividad = new Actividad();
+    $actividad->setActividades();
+}else{
+    http_response_code(400);
+    echo json_encode(['error' => true, 'mensaje' => 'ID inválido']);
+}
